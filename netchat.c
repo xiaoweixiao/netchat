e457ad36c52d7b281d0805ebed7ac72d8c1775fe
xiaoweixiao <1679204951@qq.com>
2020-02-14 17:50:56 +0800

    for test

--- netchat.c
+++ netchat.c
@@ -23,7 +23,7 @@ int main(int argc,char* argv[])
     struct sockaddr_in local;
     local.sin_family = AF_INET;
     local.sin_port = htons(atoi(argv[1]));
-    local.sin_addr.s_addr = inet_addr(INADDR_ANY);
+    local.sin_addr.s_addr = INADDR_ANY;
 
     socklen_t sock_len = sizeof(struct sockaddr_in);
     int ret = bind(sock,(struct sockaddr*)&local,sock_len);
@@ -31,40 +31,30 @@ int main(int argc,char* argv[])
         perror("bind error");
         return -1;
     }
-    printf("bind ok\n");
+
+    //connect peer
+    printf("chat with?");
+    char ip[24] = {0};
+    char port[10] = {0};
+    scanf("%s%s",ip,port);
+
+    struct sockaddr_in peer;
+    peer.sin_family = AF_INET;
+    peer.sin_port = htons(atoi(port));
+    peer.sin_addr.s_addr = inet_addr(ip);
+
     while(1){
-        //connect peer
-        printf("chat with?");
-        char ip[24] = {0};
-        char port[10] = {0};
-        scanf("%s%s",ip,port);
-
-        struct sockaddr_in peer;
-        peer.sin_family = AF_INET;
-        peer.sin_port = htons(atoi(port));
-        peer.sin_addr.s_addr = inet_addr(ip);
-
-        int sock1=connect(sock,(struct sockaddr*)&peer,sock_len);
-        if(sock1 < 0){
-            perror("Please enter correct ip and port!");
+        char mess[1024] = {0};
+        printf("your message:");
+        scanf("%s",mess);
+        sendto(sock,mess,sizeof(mess),0,(struct sockaddr*)&peer,sock_len);
+        memset(mess,0x00,1024);
+        ret = recvfrom(sock,mess,sizeof(mess),0,(struct sockaddr*)&peer,&sock_len);
+        if(ret < 0){
+            perror("recv error");
             continue;
         }
-        
-        printf("connect ok\n");
-        while(1){
-            char mess[1024] = {0};
-            printf("your message:");
-            scanf("%s",mess);
-            send(sock1,mess,sizeof(mess),0);
-            memset(mess,0x00,1024);
-            ret = recv(sock1,mess,1023,0);
-            if(ret < 0){
-                perror("recv error");
-                continue;
-            }
-            printf("recv message:%s\n",mess);
-        }
-        close(sock1);
+        printf("recv message:%s\n",mess);
     }
     close(sock);
 }
